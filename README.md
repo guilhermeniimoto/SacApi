3de056afc5384dea24282d06b8a9c6d89c9628c6
GUILHERME GOMES NIIMOTO <RM333745@fiap.com.br>
2019-10-22 23:37:32 -0300

    Arquivos sac host

--- README.md
+++
@@ -1,2 +0,0 @@
-# SacApi
-api do sac
---
+++ SelfHost/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Owin;
+
+namespace SacHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseErrorPage();
+            app.UseWelcomePage("/");
+        }
+    }
+}
